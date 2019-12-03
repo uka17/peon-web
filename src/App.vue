59c90ddd20aa8dd18d02bea7543d9f21621191f2
Major Tom Johnson <ukaoneseven@gmail.com>
2019-12-03 23:07:24 +0300

    Job execution function was enhanced

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,6 @@
       @vuetable:pagination-data="onPaginationData"
       :css="css.table"
     >
-      <template slot="job-name" slot-scope="props">
-        {{ props.rowData.name }}<span class="icon has-text-info" :data-tooltip="props.rowData.description"><i class="mdi mdi-information"></i></span>
-      </template>
     </vuetable>
     <div class="columns">
       <div class="column is-paddingless">
@@ -32,28 +29,13 @@ import Vuetable from 'vuetable-2'
 import VuetablePagination from '../node_modules/vuetable-2/src/components/VuetablePagination.vue'
 import VuetablePaginationInfo from '../node_modules/vuetable-2/src/components/VuetablePaginationInfo.vue'
 import vue_css from './components/vue-table-style.js'
-import utils from './components/utils.js'
-import config from './components/config.js'
+import fields_definition from './components/joblist-fields-defintion.js'
 
 export default {
   data () {
     return {
       css: vue_css,
-      fields:
-        [
-          {name: '__slot:job-name', title: 'Name', sortField: 'name'},                    
-          {name: 'enabled', sortField: 'enabled', title: 'Enabled'},
-          {name: 'status', sortField: 'status', title: 'Status'},
-          {name: 'last_run_result', sortField: 'last_run_result', title: 'Last run', callback: this.runResult},
-          {name: 'last_run_on', sortField: 'last_run_on', title: 'Last run on', callback: this.formatDateTime},
-          {name: 'next_run', sortField: 'next_run', title: 'Next run', callback: this.formatDateTime},
-          {name: 'step_count', sortField: 'step_count', title: 'Steps'},
-          {name: 'schedule_count', sortField: 'schedule_count', title: 'Schedules'},
-          {name: 'created_by', sortField: 'created_by', title: 'Created by'},
-          {name: 'created_on', sortField: 'created_on', title: 'Created', callback: this.formatDateTime},
-          {name: 'modified_by', sortField: 'created_by', title: 'Modified by'},
-          {name: 'modified_on', sortField: 'modified_on', title: 'Modified', callback: this.formatDateTime}
-        ]
+      fields: fields_definition        
     }
   },
   methods: {
@@ -63,13 +45,6 @@ export default {
     },
     onChangePage (page) {
       this.$refs.vuetable.changePage(page)
-    },
-    formatDateTime: (v) => utils.formatDateTime(v, config.tableDateTimeFormat, 'en'),
-    runResult (v) {
-      let tooltip = v ? 'Success' : 'Failure';
-      let icon = v ? 'mdi mdi-checkbox-marked-circle' : 'mdi mdi-close-circle';
-      let color = v ? 'icon has-text-success' : 'icon has-text-danger';
-      return `<span class="${color}" data-tooltip="${tooltip}"><i class="${icon}"></i></span>`
     }
   },
   components: {
